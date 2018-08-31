a53a377092f036677d141c811c7f43bb3dc56f66
Jamie Healey <jamie.healey@dubitlimited.com>
2018-08-31 17:32:59 +0100

    first pass at template repo

---
+++ Tests/ExampleScript.Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Duck.MyPackage.Tests
+{
+	// Just an example script to show the correct namespaces
+	[TestFixture]
+	public class ExampleScript
+	{
+		[Test]
+		public void Expect_SomethingCool_ToHappen()
+		{
+			Assert.Fail("You have not written any tests!");
+		}
+	}
+}
